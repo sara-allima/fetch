a9aa420b8484af46dc926cd8682d2cfc2ad618c0
Sara Lima <saramalima82@gmail.com>
2025-05-10 19:36:17 -0300

    Métodos POST, PATCH e DELETE

--- methods/js/script.js
+++ methods/js/script.js
@@ -18,3 +18,45 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     //const container = document.getElementById('container')
   });
 */
+
+// POST
+/*
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: 'Hello',
+        body: 'This it', 
+        userId: 10
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+})
+    .then(response => response.json())
+    .then(post => {
+        const container = document.getElementById('container')
+        container.innerHTML = `Título: ${post.title}<br>
+        Conteúdo: ${post.body}<br>
+        Id de Usuário: ${post.userId}`
+    })
+*/
+
+// PATCH
+/*
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    body: JSON.stringify({
+        title: 'foo'
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+})
+    .then(response => response.json())
+    .then(posts => console.log(posts))
+*/
+
+// DELETE
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'DELETE'
+})
